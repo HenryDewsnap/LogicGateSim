8a52300ecad3786e04e33d652036007f443eeae6
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-12 21:25:04 +0000

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import programInterpreter
+
+def main():
+    LGSInterpreter = logicGateSim.programInterpreter.interpreter("", ["xor", "not", "and", "or"])
+    
+    LGSInterpreter.createVariable({"name":"var1", "value":[0]})
+    LGSInterpreter.createVariable({"name":"var44", "value":[1]})
+    LGSInterpreter.executeGate({"gate":"xor", "inputs":"var1,var44", "outputs":"var2"})
+    LGSInterpreter.executeGate({"gate":"not", "inputs":"var2", "outputs":"var3"})
+
+
+    print(LGSInterpreter.memory['var1'])
+    print(LGSInterpreter.memory['var2'])
+
+if __name__ == "__main__":
+    main()
